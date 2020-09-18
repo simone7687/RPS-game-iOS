68fd2f45e2934e482cbbba711373f03e3cd233f1
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2020-09-18 10:42:30 +0200

    RPS View Controller

--- rps-game/scene/RPSViewController.swift
+++ rps-game/scene/RPSViewController.swift
@@ -20,12 +20,15 @@ class RPSViewController: UIViewController {
     @IBOutlet weak var myChoiceLabel: UILabel!
     @IBAction func clickPaper(_ sender: Any) {
         myChoiceLabel.text = "🖐️"
+        Play(myChoice: Choice.Paper)
     }
     @IBAction func clickScissors(_ sender: Any) {
         myChoiceLabel.text = "✌️"
+        Play(myChoice: Choice.Scissors)
     }
     @IBAction func clickRock(_ sender: Any) {
         myChoiceLabel.text = "✊"
+        Play(myChoice: Choice.Rock)
     }
 
     var textColorNormal = UIColor.black
@@ -33,7 +36,7 @@ class RPSViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textColorNormal = resultLabel.tintColor
+        textColorNormal = resultLabel.textColor
         let randomInt = Int.random(in: 0..<3)
         
         switch randomInt {
@@ -45,6 +48,62 @@ class RPSViewController: UIViewController {
             opponentChoiceLabel.text = "👩‍🦰"
         default:
             opponentChoiceLabel.text = ""
+        }
+    }
+
+    func randomChoice() -> Choice {
+        let randomInt = Int.random(in: 0..<3)
+        switch randomInt {
+        case 0:
+            opponentChoiceLabel.text = "🖐️"
+            return Choice.Paper
+        case 1:
+            opponentChoiceLabel.text = "✌️"
+            return Choice.Scissors
+        case 2:
+            opponentChoiceLabel.text = "✊"
+            return Choice.Rock
+        default:
+            return Choice.Paper
+        }
+    }
+
+    func Play(myChoice : Choice)
+    {
+        let rivalChoice = randomChoice()
+        var win = false
+
+        if (rivalChoice == myChoice) {
+            resultLabel.text = "Equal."
+            resultLabel.textColor = textColorNormal
+            return
+        }
+        else if (myChoice == Choice.Rock && rivalChoice == Choice.Scissors) {
+            win = true
+        }
+        else if (myChoice == Choice.Rock && rivalChoice == Choice.Paper) {
+            win = false
+        }
+        else if (myChoice == Choice.Scissors && rivalChoice == Choice.Paper) {
+            win = true
+        }
+        else if (myChoice == Choice.Scissors && rivalChoice == Choice.Rock) {
+            win = false
+        }
+        else if (myChoice == Choice.Paper && rivalChoice == Choice.Rock) {
+            win = true
+        }
+        else if (myChoice == Choice.Paper && rivalChoice == Choice.Scissors) {
+            win = false
+        }
+
+        if (win) {
+            resultLabel.text = "Good, you won."
+            resultLabel.textColor = UIColor.green
+        }
+        else {
+            resultLabel.text = "Sorry, you lost."
+            resultLabel.textColor = UIColor.red
         }
     }
 }
