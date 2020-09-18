dc4e2b32f44431774d4a2646e8766af1e747eafd
Simone Massaro <49060178+simone7687@users.noreply.github.com>
2020-09-18 10:32:45 +0200

    opponent

--- rps-game/scene/RPSViewController.swift
+++ rps-game/scene/RPSViewController.swift
@@ -8,18 +8,43 @@
 
 import UIKit
 
+enum Choice {
+    case Rock
+    case Paper
+    case Scissors
+}
+
 class RPSViewController: UIViewController {
     @IBOutlet weak var opponentChoiceLabel: UILabel!
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var myChoiceLabel: UILabel!
     @IBAction func clickPaper(_ sender: Any) {
+        myChoiceLabel.text = "🖐️"
     }
     @IBAction func clickScissors(_ sender: Any) {
+        myChoiceLabel.text = "✌️"
     }
     @IBAction func clickRock(_ sender: Any) {
+        myChoiceLabel.text = "✊"
     }
+
+    var textColorNormal = UIColor.black
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textColorNormal = resultLabel.tintColor
+        let randomInt = Int.random(in: 0..<3)
+        
+        switch randomInt {
+        case 0:
+            opponentChoiceLabel.text = "🧟"
+        case 1:
+            opponentChoiceLabel.text = "👨"
+        case 2:
+            opponentChoiceLabel.text = "👩‍🦰"
+        default:
+            opponentChoiceLabel.text = ""
+        }
     }
 }
